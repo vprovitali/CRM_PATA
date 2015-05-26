b41ab9f00cf2778c139d057d4738f636cf92fefe
Valentina Provitali <valentinaprovitalic@gmail.com>
2015-05-26 13:17:46 -0500

    ejercicios varios en clase

---
+++ app/controllers/gestor_controller.rb
@@ -0,0 +1,2 @@
+class GestorController < ApplicationController
+end
---
+++ test/controllers/gestor_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GestorControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
